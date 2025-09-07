9e4a93844cdd1615c928340a2e5fc4dd90d0c038
DylanTube <dylanneymarmontalvo2014@gmail.com>
2025-09-07 13:30:08 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -3,9 +3,9 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Redirección por Sistema Operativo</title>
+  <title>Redirección por dispositivo</title>
   <script>
-    function redirigirPorSO() {
+    function redirigirPorDispositivo() {
       const ua = navigator.userAgent || navigator.vendor || window.opera;
 
       // iOS
@@ -13,28 +13,9 @@
         window.location.href = "https://tusitio.com/ios";
       }
       // Android
-      else if (/android/i.test(ua) || /Mobile/.test(ua)) {
-        window.location.href = "https://www.youtube.com/?app=desktop&hl=es";
-      }
-      // Windows PC
-      else if (/Win/i.test(ua)) {
-        console.log("Estás en Windows (PC), no redirigido.");
-      }
-      // Mac
-      else if (/Mac/i.test(ua)) {
-        console.log("Estás en Mac (PC), no redirigido.");
-      }
-      // Otros
-      else {
-        console.log("Sistema operativo no detectado, no redirigido.");
-      }
-    }
-
-    window.onload = redirigirPorSO;
-  </script>
-</head>
-<body>
-  <h1>Bienvenido</h1>
-  <p>Si estás en iOS o Android, serás redirigido automáticamente.</p>
-</body>
-</html>
+      else if (/android/i.test(ua)) {
+        // Detectar tablet por tamaño de pantalla
+        if (Math.min(window.screen.width, window.screen.height) >= 768) {
+          window.location.href = "https://www.youtube.com/?app=desktop&hl=es";
+        } else {
+          window.location.href = "http
